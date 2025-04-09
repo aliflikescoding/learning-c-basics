f01a77c5c4c6cadf6361422f16034f37c79d6178
Alif Wibowo <alif.likes.coding@gmail.com>
2025-04-09 07:28:45 +0700

    Basic vars

---
+++ src/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  char name[] = "Alif";
+  int age = 20;
+  float gpa = 3.5;
+  char avg_grade = 'A';
+
+  printf("Hello %s welcome ot the program\n", name);
+  printf("You are %d years old.\n", age);
+  printf("And your gpa is %f", gpa);
+  printf("And your average age is %c", avg_grade);
+
+  return 0;
+}
